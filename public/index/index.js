eb2a1ab528007657b117cc945903c93a8b29e4ab
Tomoro0726 <tomoro0726@gmail.com>
2021-12-10 22:26:16 +0900

    キーフレーム頑張った

--- public/index/index.js
+++ public/index/index.js
@@ -2,18 +2,20 @@ anime({
   targets: '.text1',
   opacity: [{
       value: '0',
-      duration: 0,
-      delay: 0,
+      duration: 600,
+      delay: 200,
+      easing: 'linear'
     },
     {
       value: '1',
-      duration: 1300,
-      delay: 1000,
+      duration: 600,
+      delay: 600,
+      easing: 'linear'
     },
     {
       value: '0',
       duration: 1000,
-      delay: 1300,
+      delay: 600,
       easing: 'linear'
     },
   ],
@@ -22,18 +24,20 @@ anime({
   targets: '.text2',
   opacity: [{
       value: '0',
-      duration: 0,
-      delay: 0,
+      duration: 600,
+      delay: 2600,
+      easing: 'linear'
     },
     {
       value: '1',
-      duration: 1300,
-      delay: 4800,
+      duration: 600,
+      delay: 600,
+      easing: 'linear'
     },
     {
       value: '0',
       duration: 1000,
-      delay: 1300,
+      delay: 600,
       easing: 'linear'
     },
   ],
@@ -48,7 +52,7 @@ anime({
     {
       value: '1',
       duration: 1300,
-      delay: 8300,
+      delay: 6000,
     },
   ],
 });
@@ -62,7 +66,7 @@ anime({
     {
       value: '1',
       duration: 1300,
-      delay: 8300,
+      delay: 6000,
     },
   ],
 });
@@ -76,7 +80,7 @@ anime({
     {
       value: '1',
       duration: 1300,
-      delay: 8300,
+      delay: 6000,
     },
   ],
 });
